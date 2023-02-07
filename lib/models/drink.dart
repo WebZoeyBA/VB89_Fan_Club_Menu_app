a77340a4561385eb5d6e2b97acbda28cb67e081e
WebZoeyBA <sehic.70230@cyberdude.com>
2023-02-07 16:14:45 +0100

    added images

--- lib/models/drink.dart
+++ lib/models/drink.dart
@@ -13,34 +13,72 @@ class Drink {
 }
 
 List<Drink> drinks = [
-  Drink(title: "Coffee Deluxe", img: 'images/', price: '1.00 BAM', unit: 'cup'),
   Drink(
-      title: "Nesscafe Classic",
-      img: 'images/',
+      title: "Coffee Deluxe",
+      img: 'deluxe.jpg',
       price: '1.00 BAM',
       unit: 'cup'),
-  Drink(title: "Nesscafe 3/1", img: 'images/', price: '1.00 BAM', unit: 'cup'),
-  Drink(title: "Lemonade", img: "/images", price: '1.50 BAM', unit: '0.33l'),
-  Drink(title: 'Radenska', img: 'images/', price: '1.00 BAM', unit: '0.25l'),
-  Drink(title: 'Sensation', img: 'images/', price: '1.50 BAM', unit: '0.33l'),
-  Drink(title: 'Ice Tea', img: 'images/', price: '2.00 BAM', unit: '0.25'),
-  Drink(title: "Coca Cola", img: '/images', price: '2.00 BAM', unit: '0.33l'),
-  Drink(title: "Fanta", img: '/images', price: '2.00 BAM', unit: '0.33l'),
-  Drink(title: "Sprite", img: '/images', price: '2.00 BAM', unit: '0.33l'),
-  Drink(title: "Juicy apple", img: '/images', price: '2.00 BAM', unit: '0.25l'),
   Drink(
-      title: "Juicy orange", img: '/images', price: '2.00 BAM', unit: '0.25l'),
+      title: "Nesscafe Classic",
+      img: 'classic.jpg',
+      price: '1.00 BAM',
+      unit: 'cup'),
+  Drink(title: "Nesscafe 3/1", img: '3in1.jpg', price: '1.00 BAM', unit: 'cup'),
   Drink(
-      title: "Juicy cherry", img: '/images', price: '2.00 BAM', unit: '0.25l'),
-  Drink(title: "Hell Energy", img: '/images', price: '2.00 BAM', unit: '0.25l'),
-  Drink(title: 'Tuborg', img: '/images', price: '2.50 BAM', unit: '0.33l'),
-  Drink(title: 'Somersby', img: '/images', price: '3.00 BAM', unit: '0.33l'),
-  Drink(title: "Žilavka", img: '/images', price: '2.00 BAM', unit: '0.10l'),
-  Drink(title: "Vranac", img: '/images', price: '2.00 BAM', unit: '0.10l'),
-  Drink(title: "Pelinkovac", img: 'images/', price: '1.50 BAM', unit: '0.03dl'),
-  Drink(title: "Vodka", img: 'images/', price: '1.50 BAM', unit: '0.03dl'),
-  Drink(title: "Vlahov", img: 'images/', price: '1.50 BAM', unit: '0.03dl'),
-  Drink(title: "Orahovac", img: 'images/', price: '1.50 BAM', unit: '0.03dl'),
-  Drink(title: "Gin", img: 'images/', price: '2.00 BAM', unit: '0.03dl'),
-  Drink(title: "Tekila", img: '/images', price: '2.50 BAM', unit: '0.03dl')
+      title: "Lemonade", img: "limunada.jpg", price: '1.50 BAM', unit: '0.33l'),
+  Drink(
+      title: 'Radenska', img: 'radenska.jpg', price: '1.00 BAM', unit: '0.25l'),
+  Drink(
+      title: 'Sensation',
+      img: 'senzacija.jpg',
+      price: '1.50 BAM',
+      unit: '0.33l'),
+  Drink(title: 'Ice Tea', img: 'icetea.jpg', price: '2.00 BAM', unit: '0.25'),
+  Drink(
+      title: "Coca Cola",
+      img: 'cocacola.jpg',
+      price: '2.00 BAM',
+      unit: '0.33l'),
+  Drink(title: "Fanta", img: 'fanta.jpg', price: '2.00 BAM', unit: '0.33l'),
+  Drink(title: "Sprite", img: 'sprite.jpg', price: '2.00 BAM', unit: '0.33l'),
+  Drink(
+      title: "Juicy apple",
+      img: 'jabuka.jpg',
+      price: '2.00 BAM',
+      unit: '0.25l'),
+  Drink(
+      title: "Juicy orange",
+      img: 'orange.jpg',
+      price: '2.00 BAM',
+      unit: '0.25l'),
+  Drink(
+      title: "Juicy cherry",
+      img: 'visnja.jpg',
+      price: '2.00 BAM',
+      unit: '0.25l'),
+  Drink(
+      title: "Hell Energy", img: 'hell.jpg', price: '2.00 BAM', unit: '0.25l'),
+  Drink(title: 'Tuborg', img: '', price: '2.50 BAM', unit: '0.33l'),
+  Drink(
+      title: 'Somersby', img: 'somersby.jpg', price: '3.00 BAM', unit: '0.33l'),
+  Drink(
+      title: "Graševina",
+      img: 'grasevina.jpg',
+      price: '2.00 BAM',
+      unit: '0.10l'),
+  Drink(title: "Vranac", img: 'vranac.jpg', price: '2.00 BAM', unit: '0.10l'),
+  Drink(
+      title: "Pelinkovac",
+      img: 'pelinkovac.jpg',
+      price: '1.50 BAM',
+      unit: '0.03dl'),
+  Drink(title: "Vodka", img: 'vodka.jpg', price: '1.50 BAM', unit: '0.03dl'),
+  Drink(title: "Vlahov", img: 'vlahov.jpg', price: '1.50 BAM', unit: '0.03dl'),
+  Drink(
+      title: "Orahovac",
+      img: 'orahovac.jpg',
+      price: '1.50 BAM',
+      unit: '0.03dl'),
+  Drink(title: "Gin", img: 'gin.jpg', price: '2.00 BAM', unit: '0.03dl'),
+  Drink(title: "Tekila", img: 'tekila.jpg', price: '2.50 BAM', unit: '0.03dl')
 ];
